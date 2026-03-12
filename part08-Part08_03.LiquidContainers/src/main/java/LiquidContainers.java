a29de0dbad0b4e74529c6bc3e28910d17eb7ac47
diegojumagdao <diegojumagdao@gmail.com>
2026-03-12 22:14:50 +0100

    Complete part08_03.LiquidContainers

--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,17 +5,51 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        int initialValueOne = 0;
+        int initialValueTwo = 0;
+        int boundary = 100;
+        
+        System.out.println("First: " + initialValueOne + "/" + boundary);
+        System.out.println("Second: " + initialValueTwo + "/" + boundary);
+        
         while (true) {
-            System.out.print("> ");
-
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
+            
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
 
+            if (command.equals("add") && amount >= 0) {
+                initialValueOne += amount;
+                if (initialValueOne > boundary) {
+                    initialValueOne = boundary;
+                }
+            }
+            
+            if (command.equals("move") && amount >= 0) {
+                if (amount > initialValueOne) {
+                    amount = initialValueOne;
+                }
+                initialValueOne -= amount;
+                initialValueTwo += amount;
+                if (initialValueTwo > boundary) {
+                    initialValueTwo = boundary;
+                }
+            }
+            
+            if (command.equals("remove") && amount >= 0) {
+                initialValueTwo -= amount;
+                if (initialValueTwo < 0) {
+                    initialValueTwo = 0;
+                }
+            } 
+            
+            System.out.println("");
+            System.out.println("First: " + initialValueOne + "/" + boundary);
+            System.out.println("Second: " + initialValueTwo + "/" + boundary);
         }
     }
-
 }
