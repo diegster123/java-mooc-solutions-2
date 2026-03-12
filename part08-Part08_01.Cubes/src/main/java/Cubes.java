3e5af1d9854ddded89bf557150d4350154cd14dd
diegojumagdao <diegojumagdao@gmail.com>
2026-03-12 18:12:06 +0100

    Complete part08_01.Cubes

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,17 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            
+            int value = Integer.valueOf(input);
+            int cubed = value * value * value;
+            System.out.println(cubed);
+        }
+        
     }
 }
