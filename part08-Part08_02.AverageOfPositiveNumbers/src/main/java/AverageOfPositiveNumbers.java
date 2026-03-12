e45658aabfeb9ba1090f81802cf159679b0b81aa
diegojumagdao <diegojumagdao@gmail.com>
2026-03-12 18:42:17 +0100

    Complete part08_02.AverageOfPositiveNumbers

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,26 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
+        int count = 0;
+        while (true) {
+            int value = Integer.valueOf(scanner.nextLine());
+            
+            if (value == 0) {
+                break;
+            }
+            if (value > 0) {
+                sum += value;
+                count++;
+            } else {
+                continue;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println(1.0 * sum / count);
+        }
+        
     }
 }
