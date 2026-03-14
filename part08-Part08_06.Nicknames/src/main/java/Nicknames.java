e22a7b27e56d1379640331c8b866584e9fc114c5
diegojumagdao <diegojumagdao@gmail.com>
2026-03-14 16:14:18 +0100

    Complete Part08_06.Nicknames

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,12 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("matthew", "matt");
+        hashMap.put("michael", "mix");
+        hashMap.put("arthur", "artie");
+        
+        String matt = hashMap.get("matthew");
+        System.out.println(matt);
     }
-
 }
