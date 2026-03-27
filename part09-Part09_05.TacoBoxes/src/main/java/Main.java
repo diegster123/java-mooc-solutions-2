07a9eca2dc61cf38d2ea3f58e027a9b6ef33b8ed
diegojumagdao <diegojumagdao@gmail.com>
2026-03-27 11:06:50 +0100

    Complete part09.TacoBoxes

--- part09-Part09_05.TacoBoxes/src/main/java/Main.java
+++ part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -3,5 +3,22 @@ public class Main {
 
     public static void main(String[] args) {
         // Test your code here!
+        TripleTacoBox fatass = new TripleTacoBox();
+        System.out.println(fatass.tacosRemaining());
+        fatass.eat();
+        fatass.eat();
+        fatass.eat();
+        fatass.eat();
+        System.out.println(fatass.tacosRemaining());
+        
+        CustomTacoBox customBox = new CustomTacoBox(5);
+        System.out.println(customBox.tacosRemaining());
+        customBox.eat();
+        customBox.eat();
+        customBox.eat();
+        customBox.eat();
+        customBox.eat();
+        customBox.eat();
+        System.out.println(customBox.tacosRemaining());
     }
 }
