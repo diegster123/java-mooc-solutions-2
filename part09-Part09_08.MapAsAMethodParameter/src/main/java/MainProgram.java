3532d1a0664b75bd2eb33511db2894448003f94c
diegojumagdao <diegojumagdao@gmail.com>
2026-03-29 07:29:09 +0100

    Complete part09.MapAsAMethodParameter

--- part09-Part09_08.MapAsAMethodParameter/src/main/java/MainProgram.java
+++ part09-Part09_08.MapAsAMethodParameter/src/main/java/MainProgram.java
@@ -1,12 +1,24 @@
 
+import java.util.Map;
+import java.util.HashMap;
+
+
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // test your method here
+        Map<String, String> map = new HashMap<>();
+        map.put("Diego", "Macy");
+        map.put("Number", "One");
+        
+        System.out.println(returnSize(map));
 
     }
 
     // Implement here a method returnSize, which takes a Map-object as a parameter 
     // and returns the size of the map object
+    public static int returnSize(Map<String, String> map) {
+        return map.size();
+    }
 }
