d8678f4435be7df864a49dc35c292666fdff63e5
diegojumagdao <diegojumagdao@gmail.com>
2026-03-29 08:54:57 +0100

    Complete part09.SetAsAMethodParameter

--- part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
+++ part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -1,13 +1,22 @@
-
+import java.util.Set;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
         // You can test your method here
+        Set<String> set = new HashSet<>();
+        set.add("Diego");
+        set.add("Diego");
+        set.add("Macy");
+        
+        System.out.println(returnSize(set));
 
     }
 
     // implement the method returnSize here, which returns
     // the number of elements in the set that it receives as a parameter.
-
+    public static int returnSize(Set<String> set) {
+        return set.size();
+    }
 }
