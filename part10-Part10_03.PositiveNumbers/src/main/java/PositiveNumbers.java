b58ab89594ef3af65e82e5ce37040208fdd70b31
diegojumagdao <diegojumagdao@gmail.com>
2026-04-03 13:45:58 +0100

    Simplified return statement

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -11,26 +11,25 @@ public class PositiveNumbers {
         // test your method here 
         List<Integer> numbers = new ArrayList<>();
         System.out.println("Only positive numbers will be printed. Press 0 to end program.");
-        
+
         while (true) {
             int input = Integer.valueOf(scanner.nextLine());
-            
+
             if (input == 0) {
                 break;
             }
-            
+
             numbers.add(input);
         }
-        
+
         System.out.println(positive(numbers));
     }
-    
+
     public static List<Integer> positive(List<Integer> numbers) {
-        List<Integer> values = numbers.stream()
+        return numbers.stream()
                 .filter(value -> value > 0)
                 .collect(Collectors.toList());
-        
-        return values;
+
     }
 
 }
