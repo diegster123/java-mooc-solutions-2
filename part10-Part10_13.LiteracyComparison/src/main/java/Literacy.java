3c6b413c33a8cff9b52e5ef0df02fb95d7ba8382
diegojumagdao <diegojumagdao@gmail.com>
2026-04-13 15:38:33 +0100

    Complete part10.LiteracyComparison

---
+++ part10-Part10_13.LiteracyComparison/src/main/java/Literacy.java
@@ -0,0 +1,67 @@
+
+import java.util.ArrayList;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Admin
+ */
+public class Literacy implements Comparable<Literacy> {
+
+    private String theme;
+    private String ageGroup;
+    private String gender;
+    private String country;
+    private int year;
+    private double literacy;
+
+    public Literacy(String theme, String ageGroup, String gender, String country, int year, double literacy) {
+        this.theme = theme;
+        this.ageGroup = ageGroup;
+        this.gender = gender;
+        this.country = country;
+        this.year = year;
+        this.literacy = literacy;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public String getAgeGroup() {
+        return ageGroup;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getLiteracy() {
+        return literacy;
+    }
+
+    @Override
+    public String toString() {
+        return this.country + " (" + this.year + "), " + this.gender + ", " + this.literacy;
+    }
+
+    @Override
+    public int compareTo(Literacy other) {
+        return Double.compare(this.literacy, other.literacy);
+    }
+
+}
--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,49 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class LiteracyComparison {
-    
+
     public static void main(String[] args) {
+        String file = "literacy.csv";
+        List<Literacy> list = read(file);
+        Collections.sort(list);
+        System.out.println(printLines(list));
 
     }
+
+    public static List<Literacy> read(String file) {
+        List<Literacy> list = new ArrayList<>();
+        try {
+            Files.lines(Paths.get(file)).map(line -> line.split(","))
+                    .filter(line -> line.length >= 6)
+                    .map(parts -> new Literacy(
+                            parts[0].trim(), 
+                            parts[1].trim(), 
+                            parts[2].replace("(%)", "").trim(), 
+                            parts[3].trim(), 
+                            Integer.valueOf(parts[4].trim()), 
+                            Double.valueOf(parts[5].trim())))
+                    .forEach(line -> list.add(line));
+        } catch (IOException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+          
+        return list;
+    }
+    
+    public static String printLines(List<Literacy> list) {
+//        String lines = "";
+//        for (Literacy line : list) {
+//            lines += line + "\n";
+//        }
+//        return lines;
+        return list.stream()
+                .map(Literacy::toString)
+                .reduce("", (a, b) -> a + b + "\n");
+        
+    }
+
 }
