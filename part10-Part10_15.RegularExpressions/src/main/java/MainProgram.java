4cfc727c133c359a171b66475b50700c5e07f173
diegojumagdao <diegojumagdao@gmail.com>
2026-04-14 19:23:17 +0100

    Complete part10.RegularExpressions

--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -5,5 +5,15 @@ public class MainProgram {
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        
+        Checker check = new Checker();
+        String day = "tue";
+        String vowels = "queue";
+        String date = "17:23:05";
+        
+        
+        System.out.println(check.isDayOfWeek(day));
+        System.out.println(check.allVowels(vowels));
+        System.out.println(check.timeOfDay(date));
     }
 }
