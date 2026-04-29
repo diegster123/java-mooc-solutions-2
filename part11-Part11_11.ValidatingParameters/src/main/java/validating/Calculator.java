c5f44bab999af62ff472e9da8f29e08f619ef2fe
diegojumagdao <diegojumagdao@gmail.com>
2026-04-29 13:46:14 +0100

    Complete part11.ValidatingParameters

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -3,6 +3,10 @@ package validating;
 public class Calculator {
 
     public int factorial(int num) {
+        
+        if (num < 0) {
+            throw new IllegalArgumentException("Number should not be negative.");
+        }
 
         int answer = 1;
         for (int i = 1; i <= num; i++) {
@@ -13,6 +17,13 @@ public class Calculator {
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
+        if (setSize < 0 || subsetSize < 0) {
+            throw new IllegalArgumentException("Parameters should not be negative.");
+        }
+        
+        if (subsetSize > setSize) {
+            throw new IllegalArgumentException("Subset size should not be larger than set size.");
+        }
 
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -5,7 +5,16 @@ public class Person {
     private String name;
     private int age;
 
-    public Person(String name, int age) {
+    public Person(String name, int age) {       
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name should not be empty.");
+        } else if (name.length() > 40) {
+            throw new IllegalArgumentException("Name should be less than 40 characters long.");
+        } 
+        
+        if (age > 120 || age < 0) {
+            throw new IllegalArgumentException("Age should only be between 0 and 120.");
+        }
 
         this.name = name;
         this.age = age;
