b99ed57ca81ecdaaf1b8202e88033e13514f1852
diegojumagdao <diegojumagdao@gmail.com>
2026-04-30 14:54:25 +0100

    Complete part12.Hideout

---
+++ part12-Part12_01.Hideout/src/main/java/Hideout.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Hideout<T> {
+
+    private T hideout;
+
+    public Hideout() {
+        this.hideout = null;
+    }
+
+    public void putIntoHideout(T toHide) {
+        this.hideout = toHide;
+    }
+    
+    public T takeFromHideout() {
+        if (this.hideout == null) {
+            return null;
+        } 
+        T hideoutState = null;
+        hideoutState = this.hideout;
+        this.hideout = null;
+        return hideoutState;
+    }
+    
+    public boolean isInHideout() {
+        if (this.hideout == null) {
+            return false;
+        }
+        return true;
+    }
+}
