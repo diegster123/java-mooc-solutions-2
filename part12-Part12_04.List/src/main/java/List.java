f799a2dea56b4373aea7f08b668037a6b2096f22
diegojumagdao <diegojumagdao@gmail.com>
2026-05-03 05:45:29 +0100

    Complete part12.List

---
+++ part12-Part12_04.List/src/main/java/List.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class List<T> {
+
+    private T[] array;
+    private int firstFreeIndex;
+
+    public List() {
+        this.array = (T[]) new Object[10];
+        this.firstFreeIndex = 0;
+    }
+
+    public void add(T value) {
+        if (this.array.length == this.firstFreeIndex) {
+            grow();
+        }
+        this.array[this.firstFreeIndex] = value;
+        this.firstFreeIndex++;
+    }
+
+    private void grow() {
+        int newSize = this.array.length + this.array.length / 2;
+        T[] newArray = (T[]) new Object[newSize];
+        for (int i = 0; i < this.array.length; i++) {
+            newArray[i] = this.array[i];
+        }
+
+        this.array = newArray;
+    }
+
+    public boolean contains(T value) {
+        return indexOfValue(value) >= 0;
+    }
+
+    public void remove(T value) {
+        int indexOfValue = indexOfValue(value);
+        if (indexOfValue < 0) {
+            return; // not found
+        }
+
+        moveToTheLeft(indexOfValue);
+        this.firstFreeIndex--;
+    }
+
+    public int indexOfValue(T value) {
+        for (int i = 0; i < this.firstFreeIndex; i++) {
+            if (this.array[i].equals(value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void moveToTheLeft(int fromIndex) {
+        for (int i = fromIndex; i < this.firstFreeIndex - 1; i++) {
+            this.array[i] = this.array[i + 1];
+        }
+    }
+
+    public T value(int index) {
+        if (index < 0 || index >= this.firstFreeIndex) {
+            throw new ArrayIndexOutOfBoundsException("Index " + index + " outside of [0, " + this.firstFreeIndex + "]");
+        }
+
+        return this.array[index];
+    }
+}
--- part12-Part12_04.List/src/main/java/Program.java
+++ part12-Part12_04.List/src/main/java/Program.java
@@ -6,7 +6,22 @@ public class Program {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // test your class here
-
+        List<String> strings = new List<>();
+        while (true) {
+            String input = scan.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            strings.add(input);
+        }
+        
+        while (true) {
+            String input = scan.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            System.out.println(strings.contains(input));
+        }
     }
 
 }
