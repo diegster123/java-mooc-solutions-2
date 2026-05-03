c47c2e279e5e8e9e551d28147fbb70123534162b
diegojumagdao <diegojumagdao@gmail.com>
2026-05-03 08:41:25 +0100

    Complete part12.Numbers

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,13 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("How many random numbers should be printed?");
+        int length = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < length; i++) {
+            Random numbers = new Random();
+            System.out.println(numbers.nextInt(11));
+        }
     }
 
 }
