2b0f34f39071710e32519b6e34b25aad09c0e3cf
diegojumagdao <diegojumagdao@gmail.com>
2026-05-07 13:34:06 +0100

    Complete part13.BorderPane

--- part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
+++ part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -1,11 +1,32 @@
 package borderpane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class BorderPaneApplication {
+
+public class BorderPaneApplication extends Application{
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(BorderPaneApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        BorderPane borderPane = new BorderPane();
+        
+        borderPane.setTop(new Label("NORTH"));
+        borderPane.setRight(new Label("EAST"));
+        borderPane.setBottom(new Label("SOUTH"));
+        
+        Scene scene = new Scene(borderPane);
+        
+        window.setScene(scene);
+        window.show();
+        
     }
 
 }
