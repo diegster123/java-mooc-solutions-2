9f15c46ee7eaffac8be4de4a2d63886b4c9b1e78
diegojumagdao <diegojumagdao@gmail.com>
2026-05-07 11:38:32 +0100

    Complete part13.ButtonAndTextField

--- part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
+++ part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -1,11 +1,34 @@
 package buttonandtextfield;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndTextFieldApplication {
+
+public class ButtonAndTextFieldApplication extends Application{
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndTextFieldApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+//        Label label = new Label("Name");
+        Button button = new Button("Hello");
+        TextField text = new TextField();
+        FlowPane components = new FlowPane();
+        components.getChildren().add(button);
+        components.getChildren().add(text);
+        
+        Scene scene = new Scene(components);
+        
+        window.setScene(scene);
+        window.show();
     }
 
 }
