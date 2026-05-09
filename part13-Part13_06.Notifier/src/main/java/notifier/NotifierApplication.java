86ccadaba32b7d516790ff06f4e20c7fbf4ec7a1
diegojumagdao <diegojumagdao@gmail.com>
2026-05-09 06:47:47 +0100

    Compelte part13.Notifier

--- part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,37 @@
 package notifier;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NotifierApplication {
+
+public class NotifierApplication extends Application{
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        TextField textField = new TextField();
+        Button button = new Button("Update");
+        Label label = new Label();
+        
+        button.setOnAction((event) -> {
+            label.setText(textField.getText());
+        });
+        
+        VBox box = new VBox();
+        box.getChildren().addAll(textField, button, label);
+        
+        Scene scene = new Scene(box);
+        window.setScene(scene);
+        window.show();
     }
 
 }
